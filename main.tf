eac18bc6cdc9920c67f264870226ec33d02cab65
Christian Gudbrandsen <523145@AD.REN.OSLO.KOMMUNE.NO>
2023-09-14 15:51:38 +0200

    readded acl

--- main.tf
+++ main.tf
@@ -22,7 +22,40 @@ resource "aws_s3_bucket_website_configuration" "this" {
   }
 }
 
+
+resource "aws_s3_bucket_ownership_controls" "this" {
+  bucket = aws_s3_bucket.this.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.this,
+    aws_s3_bucket_public_access_block.this
+  ]
+
+  bucket = aws_s3_bucket.this.id
+  acl    = "public-read"
+}
+
 resource "aws_s3_bucket_policy" "this" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.this,
+    aws_s3_bucket_public_access_block.this,
+    aws_s3_bucket_acl.this
+  ]
+
   bucket = aws_s3_bucket.this.id
   policy = data.template_file.website_policy.rendered
 }
